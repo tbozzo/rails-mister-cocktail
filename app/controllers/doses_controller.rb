4f8d9a5ce77538e54c82b4e40ffda14bac92c302
thomas bozzo <thomas.bozzo@me.com>
2019-08-16 11:55:35 +0200

    Initial commit

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,12 +2,12 @@ class DosesController < ApplicationController
   def index
     @doses = Dose.all
   end
-​
+
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
-​
+
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
@@ -18,13 +18,13 @@ class DosesController < ApplicationController
       redirect_to cocktails_path
     end
   end
-​
+
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
     redirect_to cocktail_path(@dose.cocktail)
   end
-​
+
   def dose_params
     # => Whitelisting
     params.require(:dose).permit(:ingredient_id, :description)
