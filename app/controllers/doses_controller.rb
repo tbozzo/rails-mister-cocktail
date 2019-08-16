c5cb9c5a8de492d303999e5d5eb5e6d84172775c
thomas bozzo <thomas.bozzo@me.com>
2019-08-16 11:15:23 +0200

    version Alice

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,32 @@
+class DosesController < ApplicationController
+  def index
+    @doses = Dose.all
+  end
+​
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+​
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      redirect_to cocktails_path
+    end
+  end
+​
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+​
+  def dose_params
+    # => Whitelisting
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
+end
